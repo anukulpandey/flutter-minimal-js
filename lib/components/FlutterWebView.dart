1278dac17cadf67ee67cfd57fce1411daccc8e09
Anukul Pandey <anukul.030601@gmail.com>
2023-04-09 01:27:21 +0530

    feat: temp fix to run js

--- lib/components/FlutterWebView.dart
+++ lib/components/FlutterWebView.dart
@@ -1,46 +1,47 @@
 import 'dart:io';
-
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 import 'dart:async';
 
 class FlutterWebView extends StatefulWidget {
+  final onWebViewCreated;
+  final controller;
+  FlutterWebView(this.onWebViewCreated,this.controller);
+
   @override
   _FlutterWebViewState createState() => _FlutterWebViewState();
 }
 
 class _FlutterWebViewState extends State<FlutterWebView> {
-  final Completer<WebViewController> _controller =
-      Completer<WebViewController>();
   bool _isWebViewReady = false;
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        body: WebView(
-          javascriptMode: JavascriptMode.unrestricted,
-          onWebViewCreated: (WebViewController webViewController) async {
-            _controller.complete(webViewController);
-            String fileJsContents =
-                await rootBundle.loadString('lib/js/dist/index.js');
-            webViewController.loadUrl(Uri.dataFromString("""
-        <script type="text/javascript">
-            $fileJsContents
-        </script>
-        """, mimeType: 'text/html').toString());
-          },
-          onPageFinished: (url) {
-            setState(() {
-              _isWebViewReady = true;
-            });
-          },
+        body: Stack(
+          children: [
+            WebView(
+              javascriptMode: JavascriptMode.unrestricted,
+              onWebViewCreated: _isWebViewReady?null: widget.onWebViewCreated,
+              onPageFinished: (url) {
+                setState(() {
+                  _isWebViewReady = true;
+                });
+              },
+            ),
+            Positioned.fill(
+              child: Container(
+                color: Colors.transparent,
+              ),
+            ),
+          ],
         ),
         floatingActionButton: ElevatedButton(
           onPressed: _isWebViewReady
               ? () {
-                  _controller.future.then((controller) {
+                  widget.controller.future.then((controller) {
                     controller
                         .runJavascriptReturningResult("helloWorld();")
                         .then((value) => print(value));
@@ -48,6 +49,19 @@ class _FlutterWebViewState extends State<FlutterWebView> {
                 }
               : null,
           child: Icon(Icons.add),
+        ),
+        // add more children here
+        bottomNavigationBar: BottomNavigationBar(
+          items: [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              label: 'Home',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.settings),
+              label: 'Settings',
+            ),
+          ],
         ),
       ),
     );
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,24 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:minimal_js_api/components/FlutterWebView.dart';
+import 'package:webview_flutter/webview_flutter.dart';
 
 void main() {
-  runApp(FlutterWebView());
+  runApp(FlutterWebView(onWebViewCreated,_controller));
 }
+
+final Completer<WebViewController> _controller =
+      Completer<WebViewController>();
+
+void onWebViewCreated (WebViewController webViewController) async {
+            _controller.complete(webViewController);
+            String fileJsContents =
+                await rootBundle.loadString('lib/js/dist/index.js');
+            webViewController.loadUrl(Uri.dataFromString("""
+        <script type="text/javascript">
+            $fileJsContents
+        </script>
+        """, mimeType: 'text/html').toString());
+          }
